bab94ebdcb5f2e0cdc27470fbc86c65cf06b4d3e
Jajodia <tjajodia@internal.tavisca.com>
2019-07-29 15:04:15 +0530

    added FileReader class

--- src/com/company/FileHandler.java
+++ src/com/company/FileHandler.java
@@ -4,13 +4,28 @@ import java.io.FileNotFoundException;
 
 public class FileHandler {
     public static void main(String[] args) {
+    fileWrite();
+    fileRead();
+    }
+
+    public static void fileWrite(){
         FileWriter fileWriter = new FileWriter();
         try {
-            String data = "This is the First Line in the File\n"+
+            String data = "This is the First Line in the File\r\n"+
                     "This is the Last line in the File";
             fileWriter.writeFile("DataFile.txt", data);
         } catch (FileNotFoundException e) {
             System.out.println("Specified File not found");
+        }
+    }
+
+    public static void fileRead(){
+        FileReader fileReader = new FileReader();
+        try {
+            String data = fileReader.read("DataFile.txt");
+            System.out.println(data);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/com/company/FileReader.java
+++ src/com/company/FileReader.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class FileReader {
 
+    public String read(String fileName) throws FileNotFoundException {
 
-    public static void main(String[] args) {
-
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        byte[] b = new byte[1024];
+        try {
+            fileInputStream.read(b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(b);
     }
 }
